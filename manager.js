50e8905f0b3624ef61047743585d7c61b90dacd3
Tsuunen <laforgue.remi@gmail.com>
2023-02-16 18:33:55 +0100

    js start

---
+++ manager.js
@@ -0,0 +1,11 @@
+// const en = document.querySelector("#enigme");
+// en.appendChild(game);
+// en.insert(game)
+
+let gameArea = document.querySelector("#game");
+
+function game1() {
+  
+}
+
+game1();

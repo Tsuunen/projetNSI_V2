d041f0097fba31bc510c7e0a8b542e123d9d579f
Tsuunen <laforgue.remi@gmail.com>
2023-02-17 16:21:44 +0100

    game 2 finished

--- manager.js
+++ manager.js
@@ -5,6 +5,10 @@ function clean() {
   container.remove();
 }
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function game1(gameArea) {
   const container = document.createElement("div");
   container.classList.add("center");
@@ -56,13 +60,13 @@ function game1(gameArea) {
       check2Container.style.display = "block";
     }
     else if (check1.checked && check2.checked) {
-      game2();
+      game2(gameArea);
     }
   });
 }
 
 function game2(gameArea) {
-  // clean();
+  clean();
 
   const container = document.createElement("div");
   container.id = "container";
@@ -72,14 +76,80 @@ function game2(gameArea) {
   const colorArea = document.createElement("div");
   colorArea.classList.add("color-area");
 
-  const btn = document.createElement("button");
-  btn.type = "button";
-  btn.innerHTML = "Commencer";
+  const btnStart = document.createElement("button");
+  btnStart.type = "button";
+  btnStart.innerHTML = "Commencer";
+
+  const btnStop = document.createElement("button");
+  btnStop.type = "button";
+  btnStop.style.display = "none";
+  btnStop.innerHTML = "Attendez ...";
 
   container.appendChild(colorArea);
-  container.appendChild(btn);
+  container.appendChild(btnStart);
+  container.appendChild(btnStop);
 
   gameArea.appendChild(container);
+
+  let ableToClick = false;
+  let isCheater = false;
+  let startTime;
+
+  btnStart.addEventListener("click", async function() {
+    colorArea.style.background = "red";
+    btnStart.style.display = "none";
+    btnStop.style.display = "block";
+    ableToClick = false;
+
+    for (let i = 0; i < Math.random() * 10000; i++) {
+      await sleep(10);
+    }
+    
+    if (!isCheater) {
+      ableToClick = true;
+      colorArea.style.background = "green";
+      btnStop.innerHTML = "Cliquez !!!";
+      startTime = new Date().getTime();
+      console.log(startTime);
+    }
+  });
+
+  btnStop.addEventListener("click", async function() {
+    if (!ableToClick) {
+      isCheater = true;
+      btnStop.innerHTML = "TRICHEUR";
+
+      await sleep(1500);
+      
+      isCheater = false;
+      colorArea.style.background = "#333";
+      btnStop.style.display = "none";
+      btnStart.style.display = "block";
+      btnStop.innerHTML = "Attendez ..."
+    }
+    else {
+      let time = new Date().getTime() - startTime;
+
+      if (time > 300) {
+        btnStop.innerHTML = "Trop lent";
+        await sleep(1500);
+
+      colorArea.style.background = "#333";
+      btnStop.style.display = "none";
+      btnStart.style.display = "block";
+      btnStop.innerHTML = "Attendez ..."
+      }
+      else {
+        btnStop.innerHTML = `${time}ms`;
+        await sleep(1500);
+        game3();
+      }
+    }
+  });
 }
 
-game2(gameArea);
+function game3() {
+  clean();
+}
+
+game1(gameArea);
